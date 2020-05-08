284937f9941a3de07a9ef6bdf35cdd42119cdc88
ryanda <ryandaputra@gmail.com>
2020-05-08 17:14:47 +0700

    json response builder

--- app/Micro/Controller.php
+++ app/Micro/Controller.php
@@ -6,11 +6,36 @@ use Phalcon\Mvc\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    const SUCCESS_TRUE = true;
+    const SUCCESS_FALSE = false;
+
     public function index()
     {
         $name = 'Hello world';
         $response = sprintf('%s at %s', $name, date('Y-m-d H:i:s'));
 
-        echo json_encode($response);
+        $this->buildResponse($response, Controller::SUCCESS_TRUE);
+    }
+
+    public function buildResponse($data = [], $success = false, $code = 400)
+    {
+        $response = [
+            'success' => $success,
+            'code' => $code,
+            'message' => "",
+            'data' => $data,
+        ];
+
+        if ($success) {
+            $response['code'] = 200;
+            $response['data'] = $data;
+        } else {
+            $response['message'] = $data;
+        }
+
+        $json = $this->response
+            ->setStatusCode($code, $response['message'])
+            ->setJsonContent($response);
+        return $json->send();
     }
 }
