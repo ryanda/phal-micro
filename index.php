bf0f1cbe2ebfea4222c172387a47e7d4c676ac97
ryanda <ryandaputra@gmail.com>
2020-05-08 17:51:52 +0700

    add unhandled err

--- index.php
+++ index.php
@@ -19,5 +19,30 @@ try {
     );
 
 } catch (\Throwable $e) {
-    echo json_encode($e->getMessage());
+    $code = 500;
+
+    $message = sprintf('An error has occurred: %s', $e->getMessage());
+
+    $response = [
+        'success' => false,
+        'code' => $code,
+        'message' => $message,
+        'data' => [],
+    ];
+
+    if (APPLICATION_ENV === 'development') {
+        $response['payload'] = [
+            'file' => basename($e->getFile()),
+            'path' => $e->getFile(),
+            'line' => $e->getLine(),
+            'query_param' => $_GET,
+            'form_data' => $_POST,
+            // 'trace' => $e->getTrace(),
+        ];
+    }
+
+    $json = $app->response
+        ->setStatusCode($code, $response['message'])
+        ->setJsonContent($response);
+    return $json->send();
 }
