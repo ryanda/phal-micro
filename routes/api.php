322c018aa3382ebf4b98dce434800b8e93fc50e6
ryanda <ryandaputra@gmail.com>
2020-05-08 17:13:36 +0700

    404 handling

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,22 @@ $index = new Collection();
 $index->setHandler(\App\Micro\Controller::class, true);
 $index->get('/', 'index');
 $app->mount($index);
+
+/**
+ * Not found handler
+ */
+$app->notFound(function () use ($app) {
+    $code = 404;
+
+    $response = [
+        'success' => false,
+        'code' => $code,
+        'message' => 'Service not found',
+        'data' => [],
+    ];
+
+    $json = $app->response
+        ->setStatusCode($code, $response['message'])
+        ->setJsonContent($response);
+    return $json->send();
+});
