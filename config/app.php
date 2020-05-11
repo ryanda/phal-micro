952ea7b4cda390514905a7cff9f638f93f52d8dd
ryanda <ryandaputra@gmail.com>
2020-05-11 16:37:15 +0700

    add cache config

--- config/app.php
+++ config/app.php
@@ -6,4 +6,19 @@ return [
         'baseUri' => getenv('APP_URL') . '/',
         'cache' => getenv('CACHE_DRIVER') ?? 'file',
     ],
+    'cache' => [
+        'driver' => getenv('CACHE_DRIVER') ?: 'stream',
+        'lifetime' => 1 * 60 * 5, // seconds
+        'serializer' => 'Json',
+        'redis' => [
+            'host' => getenv('REDIS_HOST') ?: '127.0.0.1',
+            'port' => getenv('REDIS_PORT') ?: '6379',
+            'auth' => getenv('REDIS_PASSWORD') ?: null,
+            'prefix' => 'ph-rds-',
+        ],
+        'stream' => [
+            'storageDir' => APP_PATH . '/storages/caches/',
+            'prefix' => 'ph-strm-',
+        ],
+    ],
 ];
