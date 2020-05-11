37dda66fb75d0eea16e78abd63afe08bb8017536
ryanda <ryandaputra@gmail.com>
2020-05-11 16:36:40 +0700

    add cache service

--- config/service.php
+++ config/service.php
@@ -1,6 +1,9 @@
 <?php
 
 use Phalcon\Di\FactoryDefault;
+use Phalcon\Cache\CacheFactory;
+use Phalcon\Cache\AdapterFactory;
+use Phalcon\Storage\SerializerFactory;
 
 $di = new FactoryDefault();
 
@@ -13,3 +16,39 @@ $di->setShared(
 
 $di->setShared('auth_user', null);
 
+$di->setShared(
+    'cache',
+    function () use ($config) {
+        $cacheConfig = $config->cache->toArray();
+
+        $options = [
+            'defaultSerializer' => $cacheConfig['serializer'],
+            'lifetime' => $cacheConfig['lifetime'],
+        ];
+
+        $serializerFactory = new SerializerFactory();
+        $adapterFactory    = new AdapterFactory(
+            $serializerFactory,
+            $options
+        );
+
+        $cacheFactory = new CacheFactory($adapterFactory);
+
+
+        if ($cacheConfig['driver'] == 'redis') {
+            $cacheOptions = [
+                'adapter' => 'redis',
+                'options' => $cacheConfig['redis'],
+            ];
+        } else {
+            $cacheOptions = [
+                'adapter' => 'stream',
+                'options' => $cacheConfig['stream'],
+            ];
+        }
+
+        $cache = $cacheFactory->load($cacheOptions);
+
+        return $cache;
+    }
+);
